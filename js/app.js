a1c2ce5d3a7ab5f34bdd724a0f62209d6b2bb8bf
John Meguerian <johnmeguerian@gmail.com>
2017-09-26 00:20:36 -0400

    intro block spacing and responsive sizing
    - adds script to set margin on window resize
    - sets h1 sizing to viewport units

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 console.log("Commence the hacking");
+const body = document.querySelector("body");
 
-// window.fitText(document.getElementById("headline"), 0.6);
+// Adds delay that stops event listener from firing constantly when resizing
+// Ref: https://gist.github.com/james2doyle/2c0ddd5c0aba56131f8179c1894ce72d
+function handleResize() {
+  clearTimeout(this.delayer);
+  this.delayer = setTimeout(setMargin(), 200);
+}
+
+function setMargin() {
+  const header = document.getElementById("intro");
+  const offset = header.offsetHeight;
+  const margin = window.innerHeight - offset;
+
+  header.style.marginTop = margin + "px";
+}
+
+setMargin();
+window.addEventListener("resize", handleResize);
